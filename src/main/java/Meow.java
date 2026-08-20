57cbc1bd5db8dfcee149314f0b3e1ef040d188bb
Ser Yi Zhe <isaac.ser@gmail.com>
2026-08-20 13:00:39 +0800

    Rename chatbot and add Level 0 greeting

---
+++ src/main/java/Meow.java
@@ -0,0 +1,24 @@
+public class Meow {
+    public static void main(String[] args) {
+        String banner =
+                  "███   ███ ███████  █████  ██     ██\n"
+                + "████ ████ ██      ██   ██ ██     ██\n"
+                + "██ ███ ██ █████   ██   ██ ██  █  ██\n"
+                + "██     ██ ██      ██   ██ ██ ███ ██\n"
+                + "██     ██ ███████  █████   ███ ███\n";
+
+        String greeting = "Hello! I'm Meow.\n"
+                + "How can I help you today?";
+
+        String farewell = "Bye. Hope to see you again soon!";
+
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println(greeting);
+        System.out.println(line);
+        System.out.println(farewell);
+
+    }
+}
