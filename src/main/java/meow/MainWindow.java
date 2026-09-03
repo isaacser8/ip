62dd4377e560799985549d3f8b797dff745f92d0
Ser Yi Zhe <isaac.ser@gmail.com>
2026-09-03 12:59:08 +0800

    Complete JavaFX chatbot GUI

--- src/main/java/meow/MainWindow.java
+++ src/main/java/meow/MainWindow.java
@@ -5,6 +5,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Controls the main application window.
@@ -26,8 +32,9 @@ public class MainWindow {
      */
     @FXML
     public void initialize() {
+        dialogContainer.setFillWidth(true);
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
-        dialogContainer.getChildren().add(new Label("Meow: Hello! What can I do for you? 😼"));
+        addMeowMessage("Hello! What can I do for you? 😼");
     }
 
     /**
@@ -42,10 +49,76 @@ public class MainWindow {
         }
 
         String response = meow.getResponse(input);
-
-        dialogContainer.getChildren().add(new Label("You: " + input));
-        dialogContainer.getChildren().add(new Label("Meow: " + response));
+        addUserMessage(input);
+        addMeowMessage(response);
 
         userInput.clear();
+
+        if (input.equals("bye")) {
+            Platform.exit();
+        }
+    }
+
+    /**
+     * Adds a user message to the chat window.
+     *
+     * @param message the user's message
+     */
+    private void addUserMessage(String message) {
+        Label label = new Label(message);
+        label.setWrapText(true);
+        label.setMaxWidth(280);
+        label.getStyleClass().add("user-bubble");
+
+        ImageView avatar = createAvatar("/images/user.png");
+
+        HBox container = new HBox(8, label, avatar);
+        container.setAlignment(Pos.CENTER_RIGHT);
+        container.setPadding(new Insets(5, 10, 5, 50));
+
+        dialogContainer.getChildren().add(container);
+    }
+
+    /**
+     * Adds a Meow message to the chat window.
+     *
+     * @param message Meow's response
+     */
+    private void addMeowMessage(String message) {
+        ImageView avatar = createAvatar("/images/meow.png");
+
+        Label label = new Label(message);
+        label.setWrapText(true);
+        label.setMaxWidth(280);
+        label.getStyleClass().add("meow-bubble");
+
+        HBox container = new HBox(8, avatar, label);
+        container.setAlignment(Pos.CENTER_LEFT);
+        container.setPadding(new Insets(5, 50, 5, 10));
+
+        dialogContainer.getChildren().add(container);
+    }
+
+    /**
+     * Creates a circular avatar.
+     *
+     * @param emoji the emoji to display
+     * @return the avatar label
+     */
+    /**
+     * Creates an avatar from the given image resource.
+     *
+     * @param imagePath the path to the avatar image
+     * @return the avatar image view
+     */
+    private ImageView createAvatar(String imagePath) {
+        Image image = new Image(getClass().getResourceAsStream(imagePath));
+        ImageView avatar = new ImageView(image);
+
+        avatar.setFitWidth(40);
+        avatar.setFitHeight(40);
+        avatar.setPreserveRatio(true);
+
+        return avatar;
     }
 }
